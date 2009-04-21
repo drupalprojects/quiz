a80478586075f2ff92ce5fdd4dadac3d2d1c32d5
Matt Butcher <mbutcher@201798.no-reply.drupal.org>
2009-04-21 22:06:33 +0000

    Added JavaScript for new UI.

---
+++ quiz.admin.js
@@ -0,0 +1,118 @@
+// $Id$
+
+/**
+ * Scripts for Quiz administration.
+ */
+
+var Quiz = Quiz || {};
+Quiz.reg = /^([^\[]*)\[type:([\w -_]+), id:([0-9]+)\]$/;
+
+// Key should be either always or random.
+Quiz.addQuestion = function (key) {
+  var selector = '#edit-' + key + '-autocomplete';
+  var selectedItem = $(selector).val();
+  var matches = selectedItem.match(Quiz.reg);
+  var statusCode = (key == 'always' ? 1 : 2); // e.g. QUIZ_ALWAYS, QUIZ_RANDOM
+  
+  if (matches.length < 4) {
+    return;
+  }
+  
+  var title = Drupal.checkPlain(matches[1]);
+  var type = Drupal.checkPlain(matches[2]);
+  var nid = parseInt(matches[3]);
+  var weight = 0;
+  $('.question-order-weight-' + statusCode).each(function () {
+    var thisVal = parseInt($(this).val());
+    if (thisVal > weight) {
+      weight = thisVal;
+    }
+  });
+  ++weight;
+  
+  var out = Drupal.theme('addQuestion', nid, type, title, key, weight);
+  $('#questions-order-' + statusCode + ' tr:last').after(out);
+  
+  var newRow = $('#questions-order-' + statusCode + ' tr:last').get(0);
+  
+  var table = Drupal.tableDrag['questions-order-' + statusCode];
+  table.makeDraggable(newRow);
+  if (table.changed == false) {
+    table.changed = true;
+    $(Drupal.theme('tableDragChangedWarning')).insertAfter(table.table).hide().fadeIn('slow');
+  }
+  $(selector).val(''); // Zero out the value
+  Drupal.attachBehaviors();
+}
+
+Drupal.behaviors.attachRemoveAction = function () {
+  $('.rem-link').click(function (e) {
+    var $this = $(this);
+    var remID = $this.parents('tr').find('.question-order-weight').attr('id');
+     
+    var matches = remID.match(/edit-weights-([a-zA-Z]+)-([0-9]+)/);
+    if (matches.length < 3) {
+      return false;
+    }
+    
+    var remItem = matches[1] + '-' + matches[2];
+    
+    var statusCode = (matches[1] == 'always') ? 1 : 0;
+    
+    var remList = $('#edit-remove-from-quiz');
+    var orig = remList.val();
+    remList.val(remItem + ',' + orig);
+    
+    $this.parents('tr').remove();
+    
+    var table = Drupal.tableDrag['questions-order-' + statusCode];
+    if (!table.changed) {
+      table.changed;
+      $(Drupal.theme('tableDragChangedWarning')).insertAfter(table.table).hide().fadeIn('slow');
+    }
+    
+    e.preventDefault();
+    return true;
+  })
+};
+
+Drupal.theme.prototype.addQuestion = function (nid, type, title, state, weight) {
+  var fieldID = state + '-' + nid;
+  var pre = (location.search.indexOf('?q=') == 0 ? '?q=' : Drupal.settings.basePath);
+  var nodeLink = pre + Drupal.encodeURIComponent('node/' + nid);
+  var remLink = pre + Drupal.encodeURIComponent('node/' + nid + '/questions/remove');
+  var stateCode = (state == 'always') ? 1: 2;
+  var actions = 'View'.link(nodeLink) + ' | ' + $('Remove'.link('#')).addClass('rem-link').parent().html();
+  
+  return '<tr class="draggable">' +
+    '<td>' + title + '<span class="warning tabledrag-changed">*</span></td><td>' + type + '</td><td>' + actions + 
+    '</td><td style="display:none">' +
+    '<div id="edit-weights-' + fieldID + '-wrapper" class="form-item">' +
+    '<input id="edit-weights-' + fieldID + '" class="form-text question-order-weight question-order-weight-' + stateCode + '" ' +
+    'type="text" value="' + weight + '" size="3" name="weights[' + fieldID + ']" maxlength="4"/>' +
+    '</div></td></tr>';
+}
+
+
+$(document).ready(function () {
+  
+  // Stupid hack to get around bug in tableDrag (collapsed tableDrag tables cannot have hidden Weight fields)
+  $('fieldset.collapsible:last>legend>a').click();
+  
+  // Effectively bind the autocomplete submit handler to the 
+  // "Add question" button's submit handler (for both autocomplete fields).
+  
+  $('#edit-always-autocomplete').keypress(function (e) {
+      //$('#edit-add-to-list').click(); 
+      e.preventDefault();
+      e.stopPropagation();
+      console.log('Keyup caught');
+      return false;
+  });
+  
+  $('#edit-random-autocomplete').submit(function (e) {
+    $('#edit-add-to-random').click(); 
+    e.preventDefault();
+  });
+  
+});
