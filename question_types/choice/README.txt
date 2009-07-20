347c3fa67b21e847fe502bd02b73dcae0adb4207
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-07-20 11:36:56 +0000

    feature #508744 by falcon: dev version of choice.

---
+++ question_types/choice/README.txt
@@ -0,0 +1,48 @@
+// $Id$
+
+/**
+ * @file
+ * README file for Quiz Choice.
+ */
+
+Quiz Choice
+Allows quiz creators to make a multiple choice question type.
+
+Sponsored by: Norwegian Centre for Telemedicine
+Code: falcon
+
+
+CONTENTS
+--------
+
+1.  Introduction
+2.  Installation
+3.  Configuration
+
+----
+1. Introduction
+
+This module is an attempt to make it easy to construct surveys using the quiz module.
+
+The Scale module lets the quiz creator store preset answer collections, enabling rapid
+creation of survey questions.
+
+The scale module is based on the OO framework of the quiz project.
+
+----
+2. Installation
+
+To install, unpack the module to your modules folder, and simply enable the module at Admin > Build > Modules.
+
+Several database tables prefixed with quiz_scale are installed to have a separate storage for this module.
+
+----
+3.  Configuration
+Settings are to be found here: admin/quiz/scale
+
+Preset answer collections can be configured at: scale/collection/manage
+Here you can change and delete presets(answer collections).
+
+You can give roles the 'Edit global presets' permission in the permission administration(admin/user/permissions)
+With this permission users can make presets available to all users, and not only themselves.
+
---
+++ question_types/choice/choice.js
@@ -0,0 +1,58 @@
+// $id$
+/**
+ * @file
+ * Javascript functions for the choice question type.
+ */
+
+/**
+ * Refreshes scores when the checkbox named correct is toggled.
+ * 
+ * @param selection
+ */
+function refreshScores(checkbox) {
+  var prefix = '#' + getCorrectIdPrefix(checkbox.id);
+  if (checkbox.checked) {
+    $(prefix + 'score-if-chosen').val('1');
+    $(prefix + 'score-if-not-chosen').val('0');
+  }
+  else {
+	$(prefix + 'score-if-chosen').val('0');
+	if ($('#edit-alternatives-multi').attr('checked')) {
+	  $(prefix + 'score-if-not-chosen').val('1');
+	} 
+	else {
+	  $(prefix + 'score-if-not-chosen').val('0');
+	}
+  }
+}
+function refreshCorrect(textfield) {
+  var prefix = '#' + getCorrectIdPrefix(textfield.id);
+  var chosenScore;
+  var notChosenScore;
+  if (isChosen(textfield.id)) {
+    chosenScore = new Number(textfield.value);
+    notChosenScore = new Number($(prefix + 'score-if-not-chosen').val());
+  }
+  else {
+    chosenScore = new Number($(prefix + 'score-if-chosen').val());
+    notChosenScore = new Number(textfield.value);
+  }
+  if(notChosenScore < chosenScore) {
+	
+    $(prefix + 'correct').attr('checked', true);
+  }
+  else {
+    
+    $(prefix + 'correct').attr('checked', false);
+  }
+}
+function getCorrectIdPrefix(string) {
+  var pattern = new RegExp("^(edit-alternatives-alternative[0-9]{1,2}-)(?:correct|score-if-(?:not-|)chosen)$");
+  pattern.exec(string);
+  return RegExp.lastParen;
+}
+function isChosen(string) {
+  var pattern = new RegExp("score-if-chosen$");
+  return pattern.test(string);
+}
+
