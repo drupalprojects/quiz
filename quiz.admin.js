51c12242a49159ae286676355e748b6a63357acd
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-09-09 13:02:11 +0000

    Questions that are selected are moved to question list using ajax when browser gets filtered, paged or sorted

--- quiz.admin.js
+++ quiz.admin.js
@@ -5,39 +5,26 @@
  */
 
 var Quiz = Quiz || {};
-Quiz.reg = /^([^\[]*)\[type:([\w -_]+), id:([0-9]+)\]$/;
 
 // Key should be either always or random.
-Quiz.addQuestion = function (key, rowHtml) {
-  var selector = '#edit-' + key + '-autocomplete';
-  var selectedItem = $(selector).val();
-  var matches = selectedItem.match(Quiz.reg);
+Quiz.addQuestions = function (key, rowHtml) {
   var statusCode = (key == 'always' ? 1 : 2); // e.g. QUIZ_ALWAYS, QUIZ_RANDOM
-
-  if (!matches || matches.length < 4) {
-    alert('Error: The entered question was not found.');
-    return;
-  }
   
   //Add the new row:
   $('#questions-order-' + statusCode + ' tr:last').after(rowHtml);
   
-  var newRow = $('#questions-order-' + statusCode + ' tr:last').get(0);
-  
-  //Hide weight column:
-  $('td:last',newRow).css('display', 'none');
-  
   var table = Drupal.tableDrag['questions-order-' + statusCode];
-  table.makeDraggable(newRow);
+  
+  $('.quiz-temp').each(function(){
+	//Hide weight column:
+    $('td:last', this).css('display', 'none');
+  table.makeDraggable(this);
+  });
   
   if (table.changed == false) {
     table.changed = true;
     $(Drupal.theme('tableDragChangedWarning')).insertAfter(table.table).hide().fadeIn('slow');
   }
-  $(selector).val(''); // Zero out the value
-  
-  // Change build id to the new id provided by the server:
-  $('[name="form_build_id"]').val(newBuildId);
   
   Drupal.attachBehaviors();
 };
