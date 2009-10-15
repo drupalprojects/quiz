1f379815cbbb4a4dc77b1fce78c4576252872fa5
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-10-15 15:13:27 +0000

    Continue changing the report page into a form - validate functions added...

--- question_types/short_answer/theme/short_answer_report.tpl.php
+++ question_types/short_answer/theme/short_answer_report.tpl.php
@@ -7,7 +7,7 @@ print '<b>'. t('Response') .':</b>';
 print drupal_render($form['answer']);
 
 print '<b>'. t('Score') .': </b>'. drupal_render($form['score']) .' '; 
-print t('of') .' '. drupal_render($form['max_score']) .' '. t('possible points.');
+print t('of') .' '. $form['max_score']['#value'] .' '. t('possible points.');
 if (isset($form['feedback']))
   print '<p><b>'. t('Feedback') .': </b><div class="quiz_answer_feedback">'. drupal_render($form['feedback']) .'</div></p>';
 ?>
