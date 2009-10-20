15e8b97d4efb40b5bfba1e5b87e79dfc680ec6bc
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-10-20 10:46:10 +0000

    Template file added to theme the charts page

---
+++ includes/quiz_dashboard/theme/quiz_dashboard_charts.tpl.php
@@ -0,0 +1,34 @@
+<?php 
+// $ID$
+
+/**
+ * @file
+ * Theming the charts page
+ * 
+ * Variables available:
+ * $charts (array)
+ * 
+ * The following charts are available:
+ * $charts['top_scorers'] (array or FALSE if chart doesn't exist)
+ * $charts['takeup'] (array or FALSE if chart doesn't exist)
+ * $charts['status'] (array or FALSE if chart doesn't exist)
+ * $charts['grade_range'] (array or FALSE if chart doesn't exist)
+ * 
+ * Each chart has a title, an image and an explanation like this:
+ * $charts['top_scorers']['title'] (string)
+ * $charts['top_scorers']['chart'] (string - img tag - google chart)
+ * $charts['top_scorers']['explanation'] (string)
+ */
+
+if (!function_exists('_quiz_dashboard_print_chart')) {
+  function _quiz_dashboard_print_chart(&$chart) {
+    if (is_array($chart))
+      print '<h2 class="quiz-charts-title">'. $chart['title'] .'</h2>'. $chart['chart'] . $chart['explanation'];
+  }
+}
+
+_quiz_dashboard_print_chart($charts['takeup']);
+_quiz_dashboard_print_chart($charts['top_scorers']);
+_quiz_dashboard_print_chart($charts['status']);
+_quiz_dashboard_print_chart($charts['grade_range']);
+?>
