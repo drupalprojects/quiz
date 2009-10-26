f8ec6e705015f6b5959da39487e869bceb90a580
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-10-26 12:12:25 +0000

    Small adjustment to result page template

--- theme/quiz_report_form.tpl.php
+++ theme/quiz_report_form.tpl.php
@@ -7,12 +7,14 @@
  * 
  * Available variables:
  * $form - FAPI array
+ * Spcially usefull values in the form:
+ * $form['#showpoints'] - boolean
  * 
  */
 $td_classes = array('quiz-report-odd-td', 'quiz-report-even-td');
 $td_class_i = 0;
 $p = drupal_get_path('module', 'quiz') .'/theme/';
-$q_image = $p. 'question_bg.png'
+$q_image = $p. 'question_bg.png';
 ?>
 <h2><?php print format_plural(count($questions), 'Question Result', 'Question Results');?></h2>
 <table>
@@ -20,6 +22,8 @@ $q_image = $p. 'question_bg.png'
 foreach ($form as $key => $sub_form) {
   if (!is_numeric($key) || $sub_form['#no_report'] === TRUE) continue;
   unset($form[$key]);
+  $c_class = ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong';
+  
   ?>
   <tr><td class="<?php print $td_classes[$td_class_i]?>"><table class = "quiz-report-q-header">
     <tr>
@@ -27,7 +31,7 @@ foreach ($form as $key => $sub_form) {
         <h3 class = "quiz-report-question-label"><?php print t('Question')?></h3>
       </td>
       <td class = "quiz-report-score-cell">
-        <table class = "quiz-report-q-header">
+        <table class = "quiz-report-q-header <?php print $c_class?>">
           <tr>
             <td valign = "middle" style = "padding-right:5px;" align = "right" class = "quiz-report-score-cell"><?php print t('Score')?></td>
             <td valign = "middle" style = "width:1%; padding-right:5px;" class = "quiz-report-score-cell"><?php print drupal_render($sub_form['score'])?></td>
@@ -37,13 +41,10 @@ foreach ($form as $key => $sub_form) {
       </td>
     </tr>
   </table></td></tr>
-  <tr><td style="background:#ffffff url('<?php print $q_image?>') no-repeat top right;"><div class="quiz-report-question"><?php print drupal_render($sub_form['question']);?></div></td><tr>
-  <tr><td><?php $theme = ($sub_form['#is_correct']) ? t('The response is correct') : t('The response is incorrect');
-  ?>
-  <h3><?php print t('Response')?></h3>
+  <tr><td style="background:#ffffff url('<?php print $q_image?>') no-repeat top right;"><div class="quiz-report-question"><?php print drupal_render($sub_form['question']);?></div></td>
+  <tr><td><h3><?php print t('Response')?></h3>
   
   <?php print drupal_render($sub_form['response']);
-  print($theme);
   if ($td_class_i == 1) $td_class_i = 0;
   else $td_class_i = 1;
   ?>
