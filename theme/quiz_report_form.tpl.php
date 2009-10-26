e4ba71447401e17dddb30d49ddfb9fe1b77a9128
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-10-26 14:53:59 +0000

    Added more styling to result page, and also added the is evaluated value to the form

--- theme/quiz_report_form.tpl.php
+++ theme/quiz_report_form.tpl.php
@@ -7,9 +7,15 @@
  * 
  * Available variables:
  * $form - FAPI array
- * Spcially usefull values in the form:
- * $form['#showpoints'] - boolean
  * 
+ * All questions are in form[x] where x is an integer.
+ * Useful values:
+ * $form[x]['question'] - the question as a FAPI array(usually a form field of type "markup")
+ * $form[x]['score'] - the users score on the current question.(FAPI array usually of type "markup" or "textfield")
+ * $form[x]['max_score'] - the max score for the current question.(FAPI array of type "value")
+ * $form[x]['response'] - the users response, usually a FAPI array of type markup.
+ * $form[x]['#is_correct'] - If the users response is correct(boolean)
+ * $form[x]['#is_evaluated'] - If the users response has been evaluated(boolean)
  */
 $td_classes = array('quiz-report-odd-td', 'quiz-report-even-td');
 $td_class_i = 0;
@@ -22,7 +28,7 @@ $q_image = $p. 'question_bg.png';
 foreach ($form as $key => $sub_form) {
   if (!is_numeric($key) || $sub_form['#no_report'] === TRUE) continue;
   unset($form[$key]);
-  $c_class = ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong';
+  $c_class = ($sub_form['#is_evaluated']) ? ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong' : 'q-waiting';
   
   ?>
   <tr><td class="<?php print $td_classes[$td_class_i]?>"><table class = "quiz-report-q-header">
@@ -52,3 +58,4 @@ foreach ($form as $key => $sub_form) {
 }
 ?>
 </table>
+<div style="float:right;"><?php print drupal_render($form);?></div>
