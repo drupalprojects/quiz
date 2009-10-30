13c80a70494ba9189831a36eaa4d327badfc1f7d
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-10-30 09:46:37 +0000

    Rolling back to previous version...

--- theme/quiz_report_form.tpl.php
+++ theme/quiz_report_form.tpl.php
@@ -22,44 +22,40 @@ $td_class_i = 0;
 $p = drupal_get_path('module', 'quiz') .'/theme/';
 $q_image = $p. 'question_bg.png';
 ?>
-
 <h2><?php print format_plural(count($questions), 'Question Result', 'Question Results');?></h2>
-
 <table>
-
 <?php
 foreach ($form as $key => $sub_form):
   if (!is_numeric($key) || $sub_form['#no_report'] === TRUE) continue;
   unset($form[$key]);
-  $c_class = ($sub_form['#is_evaluated']) ? ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong' : 'q-waiting';?>
-
-  <tr class="quiz-report-question">
-	<td class="quiz-report-question">
-	  <?php /* <strong><?php print t('Question')?>: </strong> */ ?>
-	  <?php print drupal_render($sub_form['question']);?>
-	</td>
-	
-	<td class="quiz-report-score <?php print $c_class?>">
-	  <span>
-	  <?php print t('Score')?>
-	  <?php print drupal_render($sub_form['score'])?>
-      <?php print t('of') .' '. $sub_form['max_score']['#value'] ?>
-	  </span>
- 	</td>
-  </tr>
-
-  <tr colspan="2">
-	<td colspan="2" class="quiz-report-answer">
-	  <strong><?php print t('Response')?></strong>
+  $c_class = ($sub_form['#is_evaluated']) ? ($sub_form['#is_correct']) ? 'q-correct' : 'q-wrong' : 'q-waiting';
   
-      <?php print drupal_render($sub_form['response']);
-      // if ($td_class_i == 1) $td_class_i = 0;
-      // else $td_class_i = 1;
-      ?>
-    </td>
-  </tr>
-<?php endforeach; ?>
-
+  ?>
+  <tr><td class="<?php print $td_classes[$td_class_i]?>"><table class = "quiz-report-q-header">
+    <tr>
+      <td valign="middle" class = "quiz-report-q-cell">
+        <h3 class = "quiz-report-question-label"><?php print t('Question')?></h3>
+      </td>
+      <td class = "quiz-report-score-cell">
+        <table class = "quiz-report-q-header <?php print $c_class?>">
+          <tr>
+            <td valign = "middle" style = "padding-right:5px;" align = "right" class = "quiz-report-score-cell"><?php print t('Score')?></td>
+            <td valign = "middle" style = "width:1%; padding-right:5px;" class = "quiz-report-score-cell"><?php print drupal_render($sub_form['score'])?></td>
+            <td valign = "middle" align = "left" class = "quiz-report-score-cell"><?php print t('of') .' '. $sub_form['max_score']['#value'] ?></td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table></td></tr>
+  <tr><td style="background:#ffffff url('<?php print $q_image?>') no-repeat top right;"><div class="quiz-report-question"><?php print drupal_render($sub_form['question']);?></div></td></tr>
+  <tr><td><h3><?php print t('Response')?></h3>
+  
+  <?php print drupal_render($sub_form['response']);
+  if ($td_class_i == 1) $td_class_i = 0;
+  else $td_class_i = 1;
+  ?>
+  </td></tr><?php 
+endforeach;
+?>
 </table>
-
 <div style="float:right;"><?php print drupal_render($form);?></div>
