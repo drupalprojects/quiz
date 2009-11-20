150a1f5da32bc9746d72369c153ff66e500313f8
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2009-11-20 15:00:17 +0000

    GUI for revision actions in place.

--- question_types/quiz_question/theme/quiz_question_revision_actions.tpl.php
+++ question_types/quiz_question/theme/quiz_question_revision_actions.tpl.php
@@ -1,4 +1,29 @@
 <?php 
 // $Id$
 ?>
-<h1>hello world</h1>
+<p><?php print $form['#intro'];?></p>
+<p><?php print $form['#desc'];?></p>
+<?php 
+$rows = array();
+foreach ($form['quizzes'] as $key => $quiz) {
+  if (!is_numeric($key)) continue;
+  $cols = array();
+  $cols[] = $quiz['#quiz_title'];
+  foreach($quiz as $sub_key => $sub_form) {
+    if (!is_numeric($sub_key)) continue;
+    $cols[] = drupal_render($sub_form);
+  }
+  $rows[] = $cols;
+}
+print theme('table', $form['#quiz_header'], $rows);
+print drupal_render($form['submit']);
+print '<br/><br/><h1>'. t('Explanation') .'</h1><br/>';
+foreach ($form['explanation'] as $explanation):
+?>
+<b><?php print $explanation['#title']?></b><br/>
+<?php print $explanation['#expl']?>
+<p><i><?php print $explanation['#practical']?></i></p>
+<?php if (isset($explanation['#warning'])):?>
+<div style="color:#dd0000;"><?php print $explanation['#warning']?></div><br/>
+<?php endif;?>
+<?php endforeach;?>
