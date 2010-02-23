3eaf11155d26a988a3e189ac896c3770518d61cd
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2010-02-23 12:55:08 +0000

    #715620 documentation

--- includes/quiz_stats/theme/quiz_stats_revision_selector.tpl.php
+++ includes/quiz_stats/theme/quiz_stats_revision_selector.tpl.php
@@ -4,7 +4,7 @@ print '<p>'. $content['explanation'] .'</p>';
 print '<p>';
 $counter = 1;
 foreach ($content['links'] as $key => $value) {
-  print ' | '. l(t('revision !num', array('!num' => $counter++)),$value);
+  print ' | '. l(t('revision !num', array('!num' => $counter++)), $value);
 }
 print ' |</p>';
 ?>
