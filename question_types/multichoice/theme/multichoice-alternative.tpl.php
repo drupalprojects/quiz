eebe06dc175425ee1393514f7379bd475fa7bf73
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2010-08-03 09:37:12 +0000

    #858272 by xMATTx: Fixed bug where drupal_add_js wasn't used to add javascript

--- question_types/multichoice/theme/multichoice-alternative.tpl.php
+++ question_types/multichoice/theme/multichoice-alternative.tpl.php
@@ -15,8 +15,7 @@ $p = drupal_get_path('module', 'multichoice');
 drupal_add_css($p .'/theme/multichoice.css', 'module', 'all');
 
 // Add script for using the entire alternative row as a button
-print "
-<SCRIPT type='text/javascript'>Drupal.behaviors.multichoiceAlternativeBehavior = function(context) {
+drupal_add_js("Drupal.behaviors.multichoiceAlternativeBehavior = function(context) {
   $('.multichoice_row')
   .filter(':has(:checkbox:checked)')
   .addClass('selected')
@@ -34,7 +33,7 @@ print "
       }
     }
   });
-};</SCRIPT>";
+};", 'inline');
 
 // We want to have the checkbox in one table cell, and the title in the next. We store the checkbox and the titles
 $options = $form['#options'];
