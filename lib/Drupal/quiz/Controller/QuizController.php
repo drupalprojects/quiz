124b3db6bcb478b2ca85392c2a0eb7fdcfa0f802
Saranraj <saranraj@knackforge.com>
2013-08-13 18:36:40 +0530

    Initial commit drupal-8 quiz

---
+++ lib/Drupal/quiz/Controller/QuizController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\quiz\Controller;
+
+use Drupal\Core\Controller\ControllerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class QuizController implements ControllerInterface {
+  
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('module_handler'));
+  }
+  
+  /**
+  * This will return the output of the foobar page.
+  */
+  public function quizSettings() {
+    
+    print_r(entity_get_bundles()); exit;
+    
+    return array(
+      '#markup' => t('This is the demo foobar page.'),
+    );
+  }
+  /**
+  * This will return the output of the foobar page.
+  */
+  public function quizReports() {
+    return array(
+      '#markup' => t('This is the demo foobar page.'),
+    );
+  }
+}
+
