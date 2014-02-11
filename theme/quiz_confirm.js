bba89fa6abb507c5a7e38640f20a15c583b22205
Pål Jørgensen <pal.jorgensen@gmail.com>
2014-02-11 11:00:09 +0100

    Made confirm box not beeing displayed when traversing backwards in a quiz

--- theme/quiz_confirm.js
+++ theme/quiz_confirm.js
@@ -1,9 +1,9 @@
 (function ($) {
   Drupal.behaviors.confirm = {
-    attach : function(context, settings) {
-      $('form.confirm').each(function() {
+    attach : function (context, settings) {
+      $('form.confirm').each( function() {
         var $form = $(this);
-        $form.submit(function(){
+        $('#edit-submit, #edit-op').click(function (event) {
           // Return false to avoid submitting if user aborts
           return confirm($form.data('confirm-message'));
         });
