4b376c2795529c8ffb57a77bfc99d956744271d2
Andy Truong <thehongtt@gmail.com>
2014-10-06 15:42:19 +0700

    Add NodeFormHelper class.

--- src/Helper/NodeHelper.php
+++ src/Helper/NodeHelper.php
@@ -3,6 +3,7 @@
 namespace Drupal\quiz\Helper;
 
 use Drupal\quiz\Helper\Node\NodeDeleteHelper;
+use Drupal\quiz\Helper\Node\NodeFormHelper;
 use Drupal\quiz\Helper\Node\NodeInsertHelper;
 use Drupal\quiz\Helper\Node\NodePresaveHelper;
 use Drupal\quiz\Helper\Node\NodeUpdateHelper;
@@ -17,6 +18,7 @@ class NodeHelper {
   private $nodeUpdateHelper;
   private $nodePresaveHelper;
   private $nodeViewHelper;
+  private $nodeFormHelper;
 
   public function getNodeDeleteHelper() {
     if (null === $this->nodeDeleteHelper) {
@@ -112,6 +114,21 @@ class NodeHelper {
 
   public function view($node, $view_mode) {
     return $this->getNodeViewHelper()->execute($node, $view_mode);
+  }
+
+  /**
+   * @return NodeFormHelper
+   */
+  public function getNodeFormHelper() {
+    if (null === $this->nodeFormHelper) {
+      $this->nodeFormHelper = new NodeFormHelper();
+    }
+    return $this->nodeFormHelper;
+  }
+
+  public function setNodeFormHelper($nodeFormHelper) {
+    $this->nodeFormHelper = $nodeFormHelper;
+    return $this;
   }
 
 }
