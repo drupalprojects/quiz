5b6beed263d621a133d77b1189a7012861eb96a5
Andy Truong <thehongtt@gmail.com>
2014-10-07 16:44:33 +0700

    Move quiz_update_quiz_question_relationship() to NodeUpdateHelper::updateQuestionRelationship()

--- src/Helper/Node/NodeUpdateHelper.php
+++ src/Helper/Node/NodeUpdateHelper.php
@@ -7,9 +7,8 @@ class NodeUpdateHelper {
   public function execute($node) {
     // Quiz node vid (revision) was updated.
     if (isset($node->revision) && $node->revision) {
-      // Create new quiz-question relation entries in the quiz_node_relationship
-      // table.
-      quiz_update_quiz_question_relationship($node->old_vid, $node->vid, $node->nid);
+      // Create new quiz-question relation entries in the quiz_node_relationship table.
+      $this->updateQuestionRelationship($node->old_vid, $node->vid, $node->nid);
     }
 
     // Update an existing row in the quiz_node_properties table.
@@ -22,6 +21,73 @@ class NodeUpdateHelper {
     _quiz_check_num_always($node);
     quiz_update_max_score_properties(array($node->vid));
     drupal_set_message(t('Some of the updated settings may not apply to quiz being taken already. To see all changes in action you need to start again.'), 'warning');
+  }
+
+  /**
+   * Copies quiz-question relation entries in the quiz_node_relationship table
+   * from an old version of a quiz to a new.
+   *
+   * @param $old_quiz_vid
+   *   The quiz vid prior to a new revision.
+   * @param $new_quiz_vid
+   *   The quiz vid of the latest revision.
+   * @param $quiz_nid
+   *   The quiz node id.
+   */
+  private function updateQuestionRelationship($old_quiz_vid, $new_quiz_vid, $quiz_nid) {
+    // query for questions in previous version
+    $result = db_select('quiz_node_relationship', 'qnr')
+      ->fields('qnr', array('parent_nid', 'child_nid', 'child_vid', 'question_status', 'weight', 'max_score', 'auto_update_max_score', 'qnr_id', 'qnr_pid'))
+      ->condition('parent_nid', $quiz_nid)
+      ->condition('parent_vid', $old_quiz_vid)
+      ->condition('question_status', QUESTION_NEVER, '!=')
+      ->execute();
+
+    // only proceed if query returned data
+    if ($result->rowCount()) {
+      $questions = $result->fetchAll(PDO::FETCH_ASSOC);
+      foreach ($questions as &$quiz_question) {
+        $quiz_question['old_qnr_id'] = $quiz_question['qnr_id'];
+        $quiz_question['parent_nid'] = $quiz_nid;
+        $quiz_question['parent_vid'] = $new_quiz_vid;
+        unset($quiz_question['qnr_id']);
+        drupal_write_record('quiz_node_relationship', $quiz_question);
+      }
+
+      // Update the parentage when a new revision is created.
+      // @todo this is copy pasta from quiz_set_questions
+      foreach ($questions as $question) {
+        db_update('quiz_node_relationship')
+          ->condition('qnr_pid', $question['old_qnr_id'])
+          ->condition('parent_nid', $quiz_nid)
+          ->condition('parent_vid', $new_quiz_vid)
+          ->fields(array('qnr_pid' => $question['qnr_id']))
+          ->execute();
+      }
+    }
+
+    /* Update terms if any */
+    $result = db_select('quiz_terms', 'qt')
+      ->fields('qt', array('nid', 'tid', 'weight', 'max_score', 'number'))
+      ->condition('vid', $old_quiz_vid)
+      ->execute();
+
+    // only proceed if query returned data
+    if ($result->rowCount()) {
+      $insert_query = db_insert('quiz_terms')
+        ->fields(array('nid', 'vid', 'tid', 'weight', 'max_score', 'number'));
+      while ($quiz_term = $result->fetchAssoc()) {
+        $insert_query->values(array(
+          'nid' => $quiz_nid,
+          'vid' => $new_quiz_vid,
+          'tid' => $quiz_term['tid'],
+          'weight' => $quiz_term['weight'],
+          'max_score' => $quiz_term['max_score'],
+          'number' => $quiz_term['number'],
+        ));
+      }
+      $insert_query->execute();
+    }
   }
 
 }
