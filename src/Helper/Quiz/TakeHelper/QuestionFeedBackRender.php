0a7172dcfc8282dc8531ddc155fd8fa1d8d9fa93
Andy Truong <thehongtt@gmail.com>
2014-10-08 17:31:37 +0700

    Move more take logic to helper classes.

--- src/Helper/Quiz/TakeHelper/QuestionFeedBackRender.php
+++ src/Helper/Quiz/TakeHelper/QuestionFeedBackRender.php
@@ -10,4 +10,37 @@ class QuestionFeedBackRender {
     $this->quiz = $quiz;
   }
 
+  public function render($question_number) {
+    if (empty($_SESSION['quiz'][$this->quiz->nid]['result_id'])) {
+      $result_id = $_SESSION['quiz']['temp']['result_id'];
+    }
+    else {
+      $result_id = $_SESSION['quiz'][$this->quiz->nid]['result_id'];
+    }
+    $quiz_result = quiz_result_load($result_id);
+    $question = node_load($quiz_result->layout[$question_number]['nid']);
+    $feedback = $this->buildRenderArray($question);
+    return $feedback;
+  }
+
+  public function buildRenderArray($question) {
+    if (empty($_SESSION['quiz'][$this->quiz->nid]['result_id'])) {
+      $result_id = $_SESSION['quiz']['temp']['result_id'];
+    }
+    else {
+      $result_id = $_SESSION['quiz'][$this->quiz->nid]['result_id'];
+    }
+
+    $types = _quiz_get_question_types();
+    $module = $types[$question->type]['module'];
+
+    // Invoke hook_get_report().
+    $report = module_invoke($module, 'get_report', $question->nid, $question->vid, $result_id);
+    require_once DRUPAL_ROOT . '/' . drupal_get_path('module', 'quiz') . '/quiz.pages.inc';
+    if ($report) {
+      $report_form = drupal_get_form('quiz_report_form', array($report));
+      return $report_form;
+    }
+  }
+
 }
