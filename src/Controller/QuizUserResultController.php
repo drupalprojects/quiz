23971bf760159d5d74a9e4eed6ad678787172101
Andy Truong <thehongtt@gmail.com>
2014-10-09 09:33:06 +0700

    Class based controller for node/%quiz_menu/quiz-results/%quiz_rid/view.

---
+++ src/Controller/QuizUserResultController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\quiz\Controller;
+
+use UnexpectedValueException;
+
+class QuizUserResultController {
+
+  /**
+   * Callback for node/%quiz_menu/quiz-results/%quiz_rid/view.
+   *
+   * Show result page for a given result id
+   *
+   * @param $result_id
+   *  Result id
+   */
+  public static function staticCallback($result) {
+    if (!$result->nid) {
+      throw new UnexpectedValueException('Invalid result.');
+    }
+
+    $result_id = $result->result_id;
+    $quiz = node_load($result->nid, $result->vid);
+    $current_quiz = node_load($result->nid);
+    $questions = _quiz_get_answers($quiz, $result_id);
+    $score = quiz_calculate_score($quiz, $result_id);
+    $summary = _quiz_get_summary_text($quiz, $score);
+    $data = array(
+      'quiz' => $quiz,
+      'questions' => $questions,
+      'score' => $score,
+      'summary' => $summary,
+      'result_id' => $result_id,
+      'account' => user_load($result->uid),
+    );
+    if (user_access('view own quiz results') || (!empty($current_quiz->review_options['end']) && array_filter($current_quiz->review_options['end']))) {
+      // User can view own quiz results OR the current quiz has "display solution".
+      return theme('quiz_result', $data);
+    }
+    else {
+      // User cannot view own results or show solution. Show summary.
+      return theme('quiz_result', $data);
+    }
+  }
+
+}
--- src/Helper/Quiz/ResultHelper.php
+++ src/Helper/Quiz/ResultHelper.php
@@ -13,31 +13,6 @@ class ResultHelper {
    *  Result id
    */
   public function renderUserResult($result_id) {
-    $result = quiz_result_load($result_id);
-
-    if ($result->nid) {
-      $quiz = node_load($result->nid, $result->vid);
-      $current_quiz = node_load($result->nid);
-      $questions = _quiz_get_answers($quiz, $result_id);
-      $score = quiz_calculate_score($quiz, $result_id);
-      $summary = _quiz_get_summary_text($quiz, $score);
-      $data = array(
-        'quiz' => $quiz,
-        'questions' => $questions,
-        'score' => $score,
-        'summary' => $summary,
-        'result_id' => $result_id,
-        'account' => user_load($result->uid),
-      );
-      if (user_access('view own quiz results') || (!empty($current_quiz->review_options['end']) && array_filter($current_quiz->review_options['end']))) {
-        // User can view own quiz results OR the current quiz has "display solution".
-        return theme('quiz_result', $data);
-      }
-      else {
-        // User cannot view own results or show solution. Show summary.
-        return theme('quiz_result', $data);
-      }
-    }
   }
 
   /**
