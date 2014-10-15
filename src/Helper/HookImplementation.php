a94f0f4ad7a486c0fe38c83d419cf661cba54ec9
Andy Truong <thehongtt@gmail.com>
2014-10-15 11:23:56 +0700

    Move entity type info definition to helper class.

--- src/Helper/HookImplementation.php
+++ src/Helper/HookImplementation.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\quiz\Helper;
 
+use Drupal\quiz\Helper\HookImplementation\HookEntityInfo;
 use Drupal\quiz\Helper\HookImplementation\HookMenu;
 
 class HookImplementation {
 
   private $hookMenu;
+  private $hookEntityInfo;
 
   /**
    * @return HookMenu
@@ -20,6 +22,21 @@ class HookImplementation {
 
   public function setHookMenu($hookMenu) {
     $this->hookMenu = $hookMenu;
+    return $this;
+  }
+
+  /**
+   * @return HookEntityInfo
+   */
+  public function getHookEntityInfo() {
+    if (null === $this->hookEntityInfo) {
+      $this->hookEntityInfo = new HookEntityInfo();
+    }
+    return $this->hookEntityInfo;
+  }
+
+  public function setHookEntityInfo($hookEntityInfo) {
+    $this->hookEntityInfo = $hookEntityInfo;
     return $this;
   }
 
---
+++ src/Helper/HookImplementation/HookEntityInfo.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace Drupal\quiz\Helper\HookImplementation;
+
+class HookEntityInfo {
+
+  public function execute() {
+    $entity_types = $this->getQuizEntity();
+
+    $entity_types += array(
+      'quiz_result'                => array(
+        'label'                  => t('Quiz result'),
+        'controller class'       => 'EntityAPIController',
+        'base table'             => 'quiz_node_results',
+        'entity keys'            => array(
+          'id' => 'result_id',
+        ),
+        'views controller class' => 'EntityDefaultViewsController',
+      ),
+      'quiz_result_answer'         => array(
+        'label'                  => t('Quiz result answer'),
+        'controller class'       => 'EntityAPIController',
+        'base table'             => 'quiz_node_results_answers',
+        'entity keys'            => array(
+          'id' => 'result_answer_id',
+        ),
+        'views controller class' => 'EntityDefaultViewsController',
+      ),
+      'quiz_question_relationship' => array(
+        'label'                  => t('Quiz question relationship'),
+        'controller class'       => 'EntityAPIController',
+        'base table'             => 'quiz_node_relationship',
+        'entity keys'            => array(
+          'id' => 'qnr_id',
+        ),
+        'views controller class' => 'EntityDefaultViewsController',
+      ),
+      // @TODO: Once quiz_entity entity type is ready, remove this entity type.
+      'quiz'                       => array(
+        'label'                  => t('Quiz properties'),
+        'controller class'       => 'EntityAPIController',
+        'base table'             => 'quiz_node_properties',
+        'entity keys'            => array('id' => 'qnp_id'),
+        'views controller class' => 'EntityDefaultViewsController',
+      ),
+    );
+
+    return $entity_types;
+  }
+
+  private function getQuizEntity() {
+    $entity_types = array();
+
+    $entity_types['quiz_entity'] = array(
+      'label'            => t('Quiz properties'),
+      'controller class' => 'EntityAPIController',
+      'description'      => t('!quiz entity', array('!quiz' => QUIZ_NAME)),
+      'entity class'     => 'Drupal\quiz\Entity\QuizEntity',
+      'base table'       => 'quiz_entity',
+      'revision table'   => 'quiz_entity_revision',
+      'fieldable'        => TRUE,
+      'entity keys'      => array('id' => 'qid', 'bundle' => 'type', 'revision' => 'vid', 'label' => 'title'),
+      'bundle keys'      => array('bundle' => 'type'),
+      'label callback'   => 'entity_class_label',
+      'uri callback'     => 'entity_class_uri',
+      'module'           => 'quiz',
+      'bundles'          => array(), # 'views controller class' => 'EntityDefaultViewsController',
+      'question'         => array('label' => t('Question'), 'custom settings' => TRUE),
+    );
+
+    $entity_types['quiz_type'] = array(
+      'label'            => t('!quiz type', array('!quiz' => QUIZ_NAME)),
+      'plural label'     => t('!quiz types', array('!quiz' => QUIZ_NAME)),
+      'description'      => t('Types of !quiz.', array('!quiz' => QUIZ_NAME)),
+      'entity class'     => 'Drupal\quiz\Entity\QuizEntityType',
+      'controller class' => 'EntityAPIControllerExportable',
+      'base table'       => 'quiz_type',
+      'fieldable'        => FALSE,
+      'bundle of'        => 'quiz_entity',
+      'exportable'       => TRUE,
+      'entity keys'      => array('id' => 'id', 'name' => 'type', 'label' => 'label'),
+      'access callback'  => 'quiz_type_access',
+      'module'           => 'quiz',
+      'admin ui'         => array(// Enable the entity API's admin UI.
+        'path'             => 'admin/structure/quiz',
+        'file'             => 'quiz.admin.inc',
+        'controller class' => 'Drupal\quiz\Entity\QuizTypeUIController',
+      ),
+    );
+
+    // Add bundle info but bypass entity_load() as we cannot use it here.
+    foreach (db_select('quiz_type', 'qt')->fields('qt')->execute()->fetchAllAssoc('type') as $type => $info) {
+      $entity_types['quiz_entity']['bundles'][$type] = array(
+        'label' => $info->label,
+        'admin' => array(
+          'path'             => 'admin/structure/quiz/manage/%quiz_type',
+          'real path'        => 'admin/structure/quiz/manage/' . $type,
+          'bundle argument'  => 4,
+          'access arguments' => array('administer quiz'),
+        ),
+      );
+    }
+
+    // Support entity cache module.
+    if (module_exists('entitycache')) {
+      $entity_types['quiz_entity']['field cache'] = FALSE;
+      $entity_types['quiz_entity']['entity cache'] = TRUE;
+    }
+
+    return $entity_types;
+  }
+
+}
