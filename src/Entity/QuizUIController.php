842a41793ac3cbcce27737b23b58043a32023cf7
Andy Truong <thehongtt@gmail.com>
2014-10-16 13:54:29 +0700

    Add action link to 'quiz/add' on 'admin/content/quiz' page.

--- src/Entity/QuizUIController.php
+++ src/Entity/QuizUIController.php
@@ -9,6 +9,11 @@ class QuizUiController extends EntityDefaultUIController {
   public function hook_menu() {
     $items = parent::hook_menu();
     $items['admin/content/quiz']['type'] = MENU_LOCAL_TASK;
+
+    // Change path from admin/content/quiz/add -> quizz/add
+    $items['quiz/add'] = $items['admin/content/quiz/add'];
+    unset($items['admin/content/quiz/add']);
+
     return $items;
   }
 
