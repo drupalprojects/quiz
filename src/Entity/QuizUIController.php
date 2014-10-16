d9fc9332baa4e0745c343f0935eccc7db1b13319
Andy Truong <thehongtt@gmail.com>
2014-10-16 09:00:32 +0700

    /admin/content/quiz as MENU_LOCAL_TASK.

--- src/Entity/QuizUIController.php
+++ src/Entity/QuizUIController.php
@@ -6,4 +6,10 @@ use EntityDefaultUIController;
 
 class QuizUiController extends EntityDefaultUIController {
 
+  public function hook_menu() {
+    $items = parent::hook_menu();
+    $items['admin/content/quiz']['type'] = MENU_LOCAL_TASK;
+    return $items;
+  }
+
 }
