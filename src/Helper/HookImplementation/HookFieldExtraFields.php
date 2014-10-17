d7f91a863a7dc6452bd454d70e957ef5bdf1339c
Andy Truong <thehongtt@gmail.com>
2014-10-17 17:00:10 +0700

    Misc: Simplify HookFieldExtraFields

--- src/Helper/HookImplementation/HookFieldExtraFields.php
+++ src/Helper/HookImplementation/HookFieldExtraFields.php
@@ -6,50 +6,8 @@ class HookFieldExtraFields {
 
   public function execute() {
     $extra['node']['quiz'] = array(
-      'display' => array(
-        'take'  => array(
-          'label'       => t('Take quiz button'),
-          'description' => t('The take button.'),
-          'weight'      => 10,
-        ),
-        'stats' => array(
-          'label'       => t('Quiz summary'),
-          'description' => t('Quiz summary'),
-          'weight'      => 9,
-        ),
-      ),
-      'form'    => array(
-        'taking'            => array(
-          'label'       => t('Taking options'),
-          'description' => t('Fieldset for customizing how a quiz is taken'),
-          'weight'      => 0,
-        ),
-        'quiz_availability' => array(
-          'label'       => t('Availability options'),
-          'description' => t('Fieldset for customizing when a quiz is available'),
-          'weight'      => 0,
-        ),
-        'summaryoptions'    => array(
-          'label'       => t('Summary options'),
-          'description' => t('Fieldset for customizing summaries in the quiz reports'),
-          'weight'      => 0,
-        ),
-        'resultoptions'     => array(
-          'label'       => t('Result options'),
-          'description' => t('Fieldset for customizing result comments in quiz reports'),
-          'weight'      => 0,
-        ),
-        'remember_settings' => array(
-          'label'       => t('Remember settings'),
-          'description' => t('Checkbox for remembering quiz settings'),
-          'weight'      => 0,
-        ),
-        'remember_global'   => array(
-          'label'       => t('Remember as global'),
-          'description' => t('Checkbox for remembering quiz settings'),
-          'weight'      => 0,
-        ),
-      ),
+      'display' => $this->getQuizDisplayFields(),
+      'form'    => $this->getQuizFormExtraFields(),
     );
 
     if ($types = quiz_get_types()) {
@@ -59,6 +17,56 @@ class HookFieldExtraFields {
     }
 
     return $extra;
+  }
+
+  private function getQuizDisplayFields() {
+    return array(
+      'take'  => array(
+        'label'       => t('Take quiz button'),
+        'description' => t('The take button.'),
+        'weight'      => 10,
+      ),
+      'stats' => array(
+        'label'       => t('Quiz summary'),
+        'description' => t('Quiz summary'),
+        'weight'      => 9,
+      ),
+    );
+  }
+
+  private function getQuizFormExtraFields() {
+    return array(
+      'taking'            => array(
+        'label'       => t('Taking options'),
+        'description' => t('Fieldset for customizing how a quiz is taken'),
+        'weight'      => 0,
+      ),
+      'quiz_availability' => array(
+        'label'       => t('Availability options'),
+        'description' => t('Fieldset for customizing when a quiz is available'),
+        'weight'      => 0,
+      ),
+      'summaryoptions'    => array(
+        'label'       => t('Summary options'),
+        'description' => t('Fieldset for customizing summaries in the quiz reports'),
+        'weight'      => 0,
+      ),
+      'resultoptions'     => array(
+        'label'       => t('Result options'),
+        'description' => t('Fieldset for customizing result comments in quiz reports'),
+        'weight'      => 0,
+      ),
+      'remember_settings' => array(
+        'label'       => t('Remember settings'),
+        'description' => t('Checkbox for remembering quiz settings'),
+        'weight'      => 0,
+      ),
+      'remember_global'   => array(
+        'label'       => t('Remember as global'),
+        'description' => t('Checkbox for remembering quiz settings'),
+        'weight'      => 0,
+      ),
+    );
   }
 
 }
