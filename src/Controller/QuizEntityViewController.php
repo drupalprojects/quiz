b25b6f8494e0f44fc066b5abde7a30ab965cd0db
Andy Truong <thehongtt@gmail.com>
2014-10-20 14:37:31 +0700

    Simplify QuizEntityViewController.

--- src/Controller/QuizEntityViewController.php
+++ src/Controller/QuizEntityViewController.php
@@ -2,34 +2,13 @@
 
 namespace Drupal\quiz\Controller;
 
-use Drupal\quiz\Entity\QuizEntity;
-
 class QuizEntityViewController {
-
-  /** @var QuizEntity */
-  private $quiz;
-
-  /** @var string */
-  private $view_mode;
-
-  /** @var string */
-  private $langCode;
-
-  public function __construct(QuizEntity $quiz, $view_mode = 'default', $langcode = NULL) {
-    $this->quiz = $quiz;
-    $this->view_mode = $view_mode;
-  }
 
   /**
    * Callback for node/%quiz_menu/take
    */
   public static function staticCallback($quiz, $view_mode = 'default', $langcode = NULL) {
-    $controller = new static($quiz, $view_mode, $langcode);
-    return $controller->render();
-  }
-
-  public function render() {
-    return '…';
+    return entity_view('quiz_entity', array($quiz), $view_mode, $langcode);
   }
 
 }
