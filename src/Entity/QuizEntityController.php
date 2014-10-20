544c673d3089707b69f16e350e96291133a8f735
Andy Truong <thehongtt@gmail.com>
2014-10-20 14:52:31 +0700

    Take aware of extra field settings.

--- src/Entity/QuizEntityController.php
+++ src/Entity/QuizEntityController.php
@@ -7,17 +7,25 @@ use EntityAPIController;
 class QuizEntityController extends EntityAPIController {
 
   public function buildContent($entity, $view_mode = 'full', $langcode = NULL, $content = array()) {
+    $extra_fields = field_extra_fields_get_display($this->entityType, $entity->type, $view_mode);
+
     // Render Stats
-    $content['quiz_entity'][$entity->qid]['stats'] = array(
-      '#markup' => @theme('quiz_view_stats', array('node' => $entity)),
-    );
+    if ($extra_fields['stats']['visible']) {
+      $content['quiz_entity'][$entity->qid]['stats'] = array(
+        '#markup' => @theme('quiz_view_stats', array('node' => $entity)),
+        '#weight' => $extra_fields['stats']['weight'],
+      );
+    }
 
     // Render take button
-    $content['quiz_entity'][$entity->qid]['take'] = array(
-      '#prefix' => '<div class="quiz-not-available">',
-      '#suffix' => '</div>',
-      '#markup' => 'quiz()->getQuizHelper()->isAvailable($quiz)',
-    );
+    if ($extra_fields['take']['visible']) {
+      $content['quiz_entity'][$entity->qid]['take'] = array(
+        '#prefix' => '<div class="quiz-not-available">',
+        '#suffix' => '</div>',
+        '#markup' => 'quiz()->getQuizHelper()->isAvailable($quiz)',
+        '#weight' => $extra_fields['take']['weight'],
+      );
+    }
 
     return parent::buildContent($entity, $view_mode, $langcode, $content);
   }
