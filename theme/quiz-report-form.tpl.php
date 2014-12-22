6d33116d1909e6beaceef5f37a2011addec742fd
devin <devin@701754.no-reply.drupal.org>
2014-12-22 11:30:58 -0500

    Issue #2397293 by djdevin: Don't display a question feedback row unless one of the question review options is enabled

--- theme/quiz-report-form.tpl.php
+++ theme/quiz-report-form.tpl.php
@@ -16,43 +16,45 @@
  * $form[x]['response'] - the users response, usually a FAPI array of type markup.
  */
 ?>
-<h2><?php print t('Question results'); ?></h2>
+<?php if (isset($form[0]['question'])): ?>
+  <h2><?php print t('Question results'); ?></h2>
+<?php endif; ?>
 <div class="quiz-report">
   <?php
   foreach ($form as $key => &$sub_form):
-    if (is_numeric($key)) {
-      if (isset($sub_form['#no_report'])) {
-        drupal_render($sub_form);
+    if (isset($sub_form['question'])):
+      if (is_numeric($key)) {
+        if (isset($sub_form['#no_report'])) {
+          drupal_render($sub_form);
+        }
       }
-    }
-    else {
-      continue;
-    }
-    ?>
-    <div class="quiz-report-row clearfix">
-      <div class="quiz-report-question dt">
-        <div class="quiz-report-question-header clearfix">
-          <?php print drupal_render($sub_form['score_display']); ?>
-          <h3><?php print t('Question') ?></h3>
-        </div>
-        <?php if (isset($sub_form['question'])): ?>
+      else {
+        continue;
+      }
+      ?>
+      <div class="quiz-report-row clearfix">
+        <div class="quiz-report-question dt">
+          <div class="quiz-report-question-header clearfix">
+            <?php print drupal_render($sub_form['score_display']); ?>
+            <h3><?php print t('Question') ?></h3>
+          </div>
           <?php print drupal_render($sub_form['question']); ?>
-        <?php endif; ?>
-      </div>
-      <?php if (isset($sub_form['response'])): ?>
-        <div class="quiz-report-response dd">
-          <h3 class="quiz-report-response-header"><?php print t('Response') ?></h3>
-          <?php print drupal_render($sub_form['response']); ?>
         </div>
-      <?php endif; ?>
-      <div class="quiz-report-question-feedback dd">
-        <?php print drupal_render($sub_form['question_feedback']); ?>
+        <?php if (isset($sub_form['response'])): ?>
+          <div class="quiz-report-response dd">
+            <h3 class="quiz-report-response-header"><?php print t('Response') ?></h3>
+            <?php print drupal_render($sub_form['response']); ?>
+          </div>
+        <?php endif; ?>
+        <div class="quiz-report-question-feedback dd">
+          <?php print drupal_render($sub_form['question_feedback']); ?>
+        </div>
+        <div class="quiz-report-score-feedback dd">
+          <?php print drupal_render($sub_form['score']); ?>
+          <?php print drupal_render($sub_form['answer_feedback']); ?>
+        </div>
       </div>
-      <div class="quiz-report-score-feedback dd">
-        <?php print drupal_render($sub_form['score']); ?>
-        <?php print drupal_render($sub_form['answer_feedback']); ?>
-      </div>
-    </div>
+    <?php endif; ?>
   <?php endforeach; ?>
   <div class="quiz-report-quiz-feedback dd">
     <?php if (isset($form['quiz_feedback']) && $form['quiz_feedback']['#markup']): ?>
