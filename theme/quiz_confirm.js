954db5164835c7c61bf88a00974aa984bbcd766c
eiriksm <eiriksm@1014468.no-reply.drupal.org>
2015-02-23 10:46:12 +0100

    Issue #2182483 by ultimike, eiriksm, sjager: Error in js confirm message - same message coming when clicking the 'Back' and the 'Finish' buttons

--- theme/quiz_confirm.js
+++ theme/quiz_confirm.js
@@ -3,9 +3,17 @@
     attach : function(context, settings) {
       $('form.quiz-answer-confirm').once(function() {
         var $form = $(this);
-        $form.submit(function(){
-          // Return false to avoid submitting if user aborts
-          return confirm($form.data('confirm-message'));
+        $form.find('#edit-submit').each(function() {
+          $(this).click(function() {
+            if ($("[name=jump_to_question]").val() == 0) {
+              return confirm($form.data('confirm-message'));
+            }
+          });
+        });
+        $form.find('#edit-op').each(function() {
+          $(this).click(function() {
+            return confirm($form.data('confirm-message'));
+          });
         });
       });
     }
