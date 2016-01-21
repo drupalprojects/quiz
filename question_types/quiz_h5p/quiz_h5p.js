bba10acd27f2de491fb971d7c765531816db8835
Svein-Tore Griff With <s.t.g.with@gmail.com>
2016-01-21 18:52:07 +0100

    Make sure we're able to retrieve the scores from iframe instances

--- question_types/quiz_h5p/quiz_h5p.js
+++ question_types/quiz_h5p/quiz_h5p.js
@@ -2,16 +2,25 @@
   $(document).ready(function() {
     if (H5P && H5P.externalDispatcher) {
       H5P.externalDispatcher.on('xAPI', function(event) {
+        // First try to get the score from the statement
         var score = event.getScore();
         var maxScore = event.getMaxScore();
+        
+        // If we didn't get it from the statement we get if from the H5P instance        
         if (score === undefined || score === null) {
+          
+          // Get the H5P content id for the question
           var contentId = event.getVerifiedStatementValue(['object', 'definition', 'extensions', 'http://h5p.org/x-api/h5p-local-content-id']);
           
-          for (var i = 0; i < H5P.instances.length; i++) {
-            if (H5P.instances[i].contentId === contentId) {
-              if (typeof H5P.instances[i].getScore === 'function') {
-                score = H5P.instances[i].getScore();
-                maxScore = H5P.instances[i].getMaxScore();
+          // If an H5P iframe exists we look there, if not it should be in our window
+          var instances = $('.h5p-iframe').length > 0 ? $('.h5p-iframe')[0].contentWindow.H5P.instances : H5P.instances;
+          
+          // Find the instance and get the score from it if possible
+          for (var i = 0; i < instances.length; i++) {
+            if (instances[i].contentId === contentId) {
+              if (typeof instances[i].getScore === 'function') {
+                score = instances[i].getScore();
+                maxScore = instances[i].getMaxScore();
               }
               break;
             }
