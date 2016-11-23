e2b665c435df9d26359e3028f4b918527a2e3943
Thomas Marstrander <thomas.marstrander@joubel.com>
2016-11-23 14:13:50 +0100

    Added true-false report

--- plugins/h5p-report/type_processors/true-false-processor.class.php
+++ plugins/h5p-report/type_processors/true-false-processor.class.php
@@ -17,6 +17,44 @@ class TrueFalseProcessor extends TypeProcessor {
    * @return string HTML for report
    */
   public function generateHTML($description, $crp, $response, $extras) {
-    return "Correct Answer: {$crp[0]}<br/>User Answer: {$response}";
+    // Add CSS
+    static $css_added;
+    if (!$css_added) {
+      drupal_add_css(drupal_get_path('module', 'h5preport') . '/styles/true-false.css');
+      $css_added = true;
+    }
+
+
+    return $this->getContent($description, $crp, $response) . $this->generateFooter();
+  }
+
+  private function getContent($description, $crp, $response) {
+    $isCorrectClass = $response === $crp[0] ?
+      'h5p-true-false-user-response-correct' :
+      'h5p-true-false-user-response-wrong';
+
+    return
+      '<div class="h5p-true-false-container">' .
+        '<p class="h5p-true-false-task-description">' . $description . '</p>' .
+        '<p class="h5p-true-false-task">' .
+          '<span class="h5p-true-false-correct-responses-pattern">' . $crp[0] . '</span>' .
+          '<span class="' . $isCorrectClass . '">' . $response . '</span>' .
+        '</p>' .
+      '</div>';
+  }
+
+  /**
+   * Generate footer,
+   *
+   * @return string
+   */
+  function generateFooter() {
+    return
+      '<div class="h5p-true-false-footer">' .
+      '<span class="h5p-true-false-correct-responses-pattern"> Correct Answer</span>' .
+      '<span class="h5p-true-false-user-response-correct"> Your correct 
+      answer</span>' .
+      '<span class="h5p-true-false-user-response-wrong"> Your incorrect answer</span>' .
+      '</div>';
   }
 }
