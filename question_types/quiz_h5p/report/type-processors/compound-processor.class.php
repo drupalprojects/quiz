c6b3117c9f294646994b4fcea2da5c7b37937bb0
Frode Petterson <frode.andre.petterson@gmail.com>
2016-11-24 11:31:03 +0100

    Re-add generic library not as submodule

---
+++ question_types/quiz_h5p/report/type-processors/compound-processor.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Class FillInProcessor
+ * Processes and generates HTML report for 'fill-in' interaction type.
+ */
+class CompoundProcessor extends TypeProcessor {
+
+  /**
+   * Determines options for interaction and generates a human readable HTML
+   * report.
+   *
+   * @param string $description Description of interaction task
+   * @param array $crp Correct responses pattern
+   * @param string $response User response
+   *
+   * @return string HTML for report
+   */
+  public function generateHTML($description, $crp, $response, $extras) {
+    $H5PReport = H5PReport::getInstance();
+
+    $reports = '';
+    if (isset($extras->children)) {
+      foreach ($extras->children as $childData) {
+        $reports .= '<div class="h5p-result">' . $H5PReport->generateReport($childData) . '</div>';
+      }
+    }
+
+    return $reports;
+  }
+}
