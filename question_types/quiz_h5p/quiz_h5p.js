24bdcb0877119c9687e0b09a6af9e92eaba08e6e
thomasmars <thomasmars87@gmail.com>
2016-11-25 18:11:11 +0100

    Store to db when navigating Quiz
    Find instance when no contentId is given

--- question_types/quiz_h5p/quiz_h5p.js
+++ question_types/quiz_h5p/quiz_h5p.js
@@ -1,5 +1,19 @@
 (function ($) {
   $(document).ready(function () {
+
+    // Store results on back, next, skip and finish
+    $('#edit-submit').click(function () {
+      storeXAPIData(getH5PInstance());
+    });
+    $('#edit-back').click(function () {
+      storeXAPIData(getH5PInstance());
+    });
+
+    // Hiding "Leave blank" button because it does the same thing as 'next'
+    // and because we can't store to database when this is clicked, so no
+    // reports will be rendered
+    $('#edit-op').css('display', 'none');
+
     if (H5P && H5P.externalDispatcher) {
       // Get xAPI data initially
       H5P.externalDispatcher.once('domChanged', function () {
@@ -16,7 +30,7 @@
   /**
    * Finds the global instance from content id by looking through the DOM
    *
-   * @param {number} contentId Content identifier
+   * @param {number} [contentId] Content identifier
    * @returns {Object} Content instance
    */
   function getH5PInstance(contentId) {
@@ -27,6 +41,23 @@
     var withContentId = function (instance) {
       return (instance.contentId === contentId);
     };
+
+    // No content id given, search for instance
+    if (!contentId) {
+      var unframedInstance = H5P.instances[0];
+      if (unframedInstance) {
+        return unframedInstance;
+      }
+      else {
+        var iframes = document.getElementsByClassName('h5p-iframe');
+
+        // Assume first iframe
+        var iframeInstance = iframes[0].contentWindow.H5P.instances[0];
+        if (iframeInstance) {
+          return iframeInstance;
+        }
+      }
+    }
 
     // Try this documents instances
     var instance = H5P.instances.find(withContentId);
