f9f0de30f478dc56130168333873b7ee9c752ed3
Devin Zuczek <dzuczek@dlc-solutions.com>
2018-04-09 11:20:03 -0400

    Issue #2953548: target skip button too

--- theme/quiz_confirm.js
+++ theme/quiz_confirm.js
@@ -1,7 +1,7 @@
 (function ($) {
   Drupal.behaviors.quizAnswerConfirm = {
     attach: function (context, settings) {
-      $('#quiz-question-answering-form #edit-navigation-submit').click(function (event) {
+      $('#quiz-question-answering-form #edit-navigation-submit, #quiz-question-answering-form #edit-navigation-skip').click(function (event) {
         // Return false to avoid submitting if user aborts
         if (!confirm($('#quiz-question-answering-form').data('confirm-message'))) {
           event.preventDefault();
